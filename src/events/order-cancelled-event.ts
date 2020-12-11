2ea1febd00de7082cf7591cc90f6307be4963927
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-12-11 00:05:26 -0500

    Updated to 1.0.24

--- src/events/order-cancelled-event.ts
+++ src/events/order-cancelled-event.ts
@@ -5,9 +5,9 @@ export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled;
   data: {
     id: string;
-    version: number;
     ticket: {
       id: string;
+      version: number;
     };
   };
 }
